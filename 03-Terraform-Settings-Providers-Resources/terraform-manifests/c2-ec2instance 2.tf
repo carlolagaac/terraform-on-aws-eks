97ce48ebeb3255a949a9efdd9c9479718b716024
carlo <carlo>
2023-04-15 18:44:11 +0800

    Remove duplicates

--- 03-Terraform-Settings-Providers-Resources/terraform-manifests/c2-ec2instance 2.tf
+++
@@ -1,9 +0,0 @@
-# Resource: EC2 Instance
-resource "aws_instance" "myec2vm" {
-  ami = "ami-0457d23bb4e5c6fd2"
-  instance_type = "t3.micro"
-  user_data = file("${path.module}/app1-install.sh")
-  tags = {
-    "Name" = "EC2 Demo"
-  }
-}
